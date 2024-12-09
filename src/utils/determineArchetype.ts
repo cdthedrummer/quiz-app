df40d1cf0e6aa136218d53eb5de4ee86a0359013
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-09 11:13:44 -0500

    Update determineArchetype.ts

--- src/utils/determineArchetype.ts
+++ src/utils/determineArchetype.ts
@@ -1,8 +1,7 @@
-import { CharacterStats } from '../types';
-import archetypes from '../data/archetypes';
+import { CharacterStats } from '@/types';
+import archetypes from '@/data/archetypes';
 
 export const determineArchetype = (stats: CharacterStats) => {
-  // Find highest stat
   const highestStat = Object.entries(stats).reduce((a, b) => 
     stats[a] > stats[b[0]] ? a : b[0]
   );
